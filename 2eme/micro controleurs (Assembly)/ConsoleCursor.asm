76fd5ca7a38d9f47859a185883ea34ab1a8e83cf
RICHEZ Antoine <richez.antoine.richez@gmail.com>
2018-12-20 19:58:44 +0100

    added asm stuff

--- 2eme/micro controleurs (Assembly)/ConsoleCursor.asm
+++ 2eme/micro controleurs (Assembly)/ConsoleCursor.asm
@@ -3,17 +3,17 @@
 section .text
 global CMAIN
 CMAIN:
-    extern SetConsoleCursorPosition
-    extern GetStdHandle
-    import SetConsoleCursorPosition kernel32.dll
-    import GetStdHandle kernel32.dll
+    extern _SetConsoleCursorPosition@8
+    extern _GetStdHandle@4
+    ;import SetConsoleCursorPosition kernel32.dll
+    ;import GetStdHandle kernel32.dll
     
     ; handle = GetStdHandle(-11)
     push dword -11
-    call [GetStdHandle]
+    call _GetStdHandle@4
     mov [handle], eax
     
-    ;PRINT_STRING "Début"
+    PRINT_STRING "Début"
     
     ; coords + call of SetConsole
     mov ax, 15 ; low order bits to new x coordinate
@@ -21,8 +21,8 @@ CMAIN:
     mov ax, 0   ; should be 0 but it's to be sure
     push eax    ; push coords to the stack
     push dword [handle] ; push the handle to the stack
-    call [SetConsoleCursorPosition]
-    ;PRINT_STRING "LOL ça marche"
+    call _SetConsoleCursorPosition@8
+    PRINT_STRING "LOL ça marche"
     
     xor eax, eax
     ret
--- 2eme/micro controleurs (Assembly)/setCursor.cpp
+++ 2eme/micro controleurs (Assembly)/setCursor.cpp
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <Windows.h>
-
+#include <iostream>
 int main(void)
 {
     COORD newPosition = {10, 20};
-
-    system("pause");
-    printf("Hello World\n");
-
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), newPosition);
-    printf("New position");
-    printf("\n");
+    int i = 0;
+    int *j = &i;
+    // system("pause");
+    // printf("Hello World\n");
+    std::cout << j << std::endl;
+    // SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), newPosition);
+    // printf("New position");
+    // printf("\n");
     return 0;
 }
 // charger une chaine de caractères (demandée à l'utilisateur) : rouge, bleu, jaune, vert
