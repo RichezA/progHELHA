4566395359d835c5bb70f0a08eeda8429778f334
Richez Antoine <richez.antoine.richez@gmail.com>
2019-03-13 23:54:09 +0100

    added the functionality of adding a message to the chatbox

--- 2eme/Programmation/Codes/Java/My codes/JavaFXLayout/src/sample/Controller.java
+++ 2eme/Programmation/Codes/Java/My codes/JavaFXLayout/src/sample/Controller.java
@@ -2,17 +2,32 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class Controller {
+
+    @FXML
+    private ScrollPane chatBox;
+
+    @FXML
+    private Label nameBar;
 
     @FXML
     private TextField answerTF;
@@ -34,17 +49,28 @@ public class Controller {
     @FXML
     void clickOnSend(ActionEvent event){
         Label label = new Label(answerTF.getText() + "\n");
-        messageBox.getChildren().clear();
-        messageBox.getChildren().add(label);
+        ImageView imgView = new ImageView("/man.png");
+        imgView.setPreserveRatio(true);imgView.setFitHeight(50);
+
+        //messageBox.getChildren().add(label);
+        FlowPane pane = new FlowPane();
+        pane.setColumnHalignment(HPos.RIGHT); pane.setAlignment(Pos.TOP_RIGHT);
+        pane.getChildren().add(label); pane.getChildren().add(imgView);
+
+
+        messageBox.getChildren().add(pane);
+
         answerTF.setText("");
+        chatBox.setVvalue(1.0);
     }
 
     @FXML
     void clickOnUser(MouseEvent event) {
         if(event.getTarget() instanceof Text){
-            messageBox.getChildren().clear();
-            Label label = new Label(((Text)event.getTarget()).getText());
-            messageBox.getChildren().add(label);
+            //messageBox.getChildren().clear();
+            //Label label = new Label(((Text)event.getTarget()).getText());
+
+            nameBar.setText(((Text) event.getTarget()).getText());
         }
 
     }
--- 2eme/Programmation/Codes/Java/My codes/JavaFXLayout/src/sample/Main.java
+++ 2eme/Programmation/Codes/Java/My codes/JavaFXLayout/src/sample/Main.java
@@ -11,7 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("CHAT2000");
+        primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
     }
 
