11a7e98355bee508e7d2ba2016421ed3295a72d7
Richez Antoine <richezatr@gmail.com>
2019-12-23 14:02:06 +0100

    added some things that i didn't remember in the course

---
+++ 3eme/Labo-Prog/WPF-XAML/CustomPgBarEventsAndTemplates/RoutedEvent/CustomProgressBar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace RoutedEventPgb
+{
+    class CustomProgressBar : ProgressBar
+    {
+        public double AlertLevel { get; set; }
+
+        public static readonly RoutedEvent AlertEvent = EventManager.RegisterRoutedEvent("Alert", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(CustomProgressBar));
+    
+        public event RoutedEventHandler Alert
+        {
+            add
+            {
+                this.AddHandler(CustomProgressBar.AlertEvent, value);
+            }
+            remove
+            {
+                this.RemoveHandler(CustomProgressBar.AlertEvent, value);
+            }
+        }
+    }
+}
---
+++ 3eme/Labo-Prog/WPF-XAML/CustomPgBarEventsAndTemplates/RoutedEvent/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RoutedEventPgb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public String Nom { get; set; }
+        public MainWindow()
+        {
+            Nom = "Antoine";
+            InitializeComponent();
+            this.Pgb.Alert += Pgb_Alert;
+            this.DataContext = this;
+        }
+
+        private void Pgb_Alert(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Alert");
+        }
+
+        // Autre façon de faire
+        private void Pgb_Alert_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("ALERT DIRECTLY FROM COMPONENT");
+        }
+    }
+}
